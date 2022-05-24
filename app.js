990f1f250d3907faa309fe3ddecd9f1358049813
foysalcodeio <md.foysalahammedforhad@gmail.com>
2022-05-24 12:38:21 -0700

    dynamic-function-done

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-function handleProductChange(IsIncrease){
-    const caseInput = document.getElementById('case-numbers');
+function handleProductChange(product, IsIncrease){
+    const caseInput = document.getElementById(product + '-numbers');
     const caseCount = parseInt(caseInput.value);
     let caseNewCount = caseCount;
     if (IsIncrease == false && caseCount > 0){
@@ -10,27 +10,49 @@ function handleProductChange(IsIncrease){
     }
     caseInput.value = caseNewCount;
     
-    const PanaTotal = caseInput.value * 600;
-    document.getElementById('pana-total').innerHTML = PanaTotal;
+    let Total = 0;
+    if (product == 'pvc'){
+        Total =  caseInput.value * 500;
+    }
+    if(product == 'pana'){
+        Total = caseInput.value * 600;
+    }
+    document.getElementById(product + '-total').innerHTML = Total;
 }
+
+// function handleProductChange(IsIncrease){
+//     const caseInput = document.getElementById('case-numbers');
+//     const caseCount = parseInt(caseInput.value);
+//     let caseNewCount = caseCount;
+//     if (IsIncrease == false && caseCount > 0){
+//         caseNewCount = caseCount - 1;
+//     }
+//     else{
+//         caseNewCount = caseCount + 1;
+//     }
+//     caseInput.value = caseNewCount;
+    
+//     const PanaTotal = caseInput.value * 600;
+//     document.getElementById('pana-total').innerHTML = PanaTotal;
+// }
 
 // onclick = document.getElementById('id').addEventListener('click', function(){.... data .... })
 
-function pvcHandleProduct(updates)
-{
-    const caseInput = document.getElementById('pvc-number');
-    const caseCount = parseInt(caseInput.value);
-    //const caseNewCount = caseCount - 1;
-    let caseNewCount = caseCount;
-    if(updates == true){
-        caseNewCount = caseCount + 1;
-    }
-    if(updates == false && caseCount > 0){
-        caseNewCount = caseCount - 1;
-    }
+// function pvcHandleProduct(updates)
+// {
+//     const caseInput = document.getElementById('pvc-number');
+//     const caseCount = parseInt(caseInput.value);
+//     //const caseNewCount = caseCount - 1;
+//     let caseNewCount = caseCount;
+//     if(updates == true){
+//         caseNewCount = caseCount + 1;
+//     }
+//     if(updates == false && caseCount > 0){
+//         caseNewCount = caseCount - 1;
+//     }
     
-    caseInput.value = caseNewCount;
+//     caseInput.value = caseNewCount;
 
-    const pvcTotal = caseInput.value * 500;
-    document.getElementById('pvc-total').innerText = pvcTotal;
-}
+//     const pvcTotal = caseInput.value * 500;
+//     document.getElementById('pvc-total').innerText = pvcTotal;
+// }
